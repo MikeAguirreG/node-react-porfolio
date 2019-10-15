fcd2b6c8edd2d64d4ffd4e7ecffcf7ff30adb137
Miguel Angel Aguirre Galvan <MiguelAguirre@Mike.local>
2019-10-15 12:53:28 +0300

    Setting portfolio thumbs nails

--- client/src/components/Page3.js
+++ client/src/components/Page3.js
@@ -2,15 +2,9 @@ import React, { useState } from 'react'
 import axios from 'axios';
 import { FaExternalLinkAlt, FaGithubSquare } from 'react-icons/fa'
 import { IconContext } from "react-icons";
-// import { DiMysql, DiMongodb, DiReact, DiNodejs, DiGit, DiJavascript1 ,DiPhp ,DiCss3,DiSass ,DiJava, DiCodeigniter, DiBootstrap } from 'react-icons/di';
-
-
-
-
 
 const Portfolio = () => {
-
-
+    //Setting up states.
     const [projects, setProjects] = useState([]);
     const fetchProjects = () => {
         axios.get('/api/catalog')
@@ -20,58 +14,40 @@ const Portfolio = () => {
     React.useEffect(() => fetchProjects(), []);
     //Icon Syzing
     const styleIcons = {
-        size: '1em'
+        size: '1.2em'
     }
     const getCatalogs = () => {
         return projects && (
             projects.map((project, index) => (
-                <div className='flex-item portfolio-item dib br3 pa2 ma2 h-40 w-20 grow bw2 shadow-6' key={index}>
-                    <article className="" >
-                        <img src={`${process.env.PUBLIC_URL}/assets/images/${project.imageName}`} className="db br2 br--top h-60" alt="Miguel Aguirre"></img>
-                        <div className="pa1 pb3-ns" style={{ overflow: 'auto' }}>
-                            <div className="dt w-100 mt1">
-                                <div className="dtc">
-                                    <h1 className="f6 f4-ns mv0">{project.name}</h1>
-                                </div>
-                            </div>
-                            <p className="f7 w-100 lh-copy measure mt2 gray">
-                                {project.description}
-                            </p>
-                            <div className="dt w-100 mt1">
-                                <div className="dtc">
-                                    <h3 className="f7 f4-ns mv0">Stack</h3>
-                                </div>
-                            </div>
-                            <p className="f7 w-100 lh-copy measure mt2 gray">
-                                {project.tecnologies.map((e) => `#${e} `)}
-                            </p>
-                            <div className="dt w-100 mt1">
-                                <div className="dtc">
-                                    <IconContext.Provider value={styleIcons}>
-                                        <a href={project.link}
-                                            target='_blank'
-                                            rel='noopener noreferrer'
-                                            className="flex-item pa2"
-                                            title='Try it alive!'
-                                        >
-                                            <FaExternalLinkAlt />
-                                        </a>
-                                        <a href={project.git}
-                                            target='_blank'
-                                            rel='noopener noreferrer'
-                                            className="flex-item pa2"
-                                            title='Check some code!'
-                                        >
-                                            <FaGithubSquare />
-                                        </a>
-                                    </IconContext.Provider>
-                                </div>
-                            </div>
-
-                        </div>
-                    </article>
+                <div className="flex-item portfolio-item dib br3 ma2 grow bw2 shadow-5" key={index}>
+                <img className='br2 h-50' src={`${process.env.PUBLIC_URL}/assets/images/${project.imageName}`} alt={project.name}/>
+                <div className='h-50 f6 f4-ns'>
+                    <h2 className='f4'>{project.name}</h2>
+                    <p className='f7 gray'>{project.description}</p>
+                    <h2 className='f5'>Stack</h2>
+                    <p className='f7 gray'>{project.tecnologies.map((e) => `#${e} `)}</p>
+                    <div className="tc">
+                         <IconContext.Provider value={styleIcons}>
+                             <a href={project.link}
+                                 target='_blank'
+                                 rel='noopener noreferrer'
+                                 className="flex-item pa2"
+                                 title='Try it alive!'
+                             >
+                                 <FaExternalLinkAlt />
+                             </a>
+                             <a href={project.git}
+                                 target='_blank'
+                                 rel='noopener noreferrer'
+                                 className="flex-item pa2"
+                                 title='Check some code!'
+                             >
+                                 <FaGithubSquare />
+                             </a>
+                         </IconContext.Provider>
+                    </div>
                 </div>
-
+            </div>
             ))
 
         )
