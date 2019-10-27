1d34ebe2706786447c2c86e4e835439b1f3a153e
Miguel Angel Aguirre Galvan <agmaug@gmail.com>
2019-10-27 19:29:35 +0200

    Adding google analytics using hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
-import React ,{ useState } from 'react';
+import React ,{ useEffect } from 'react';
 import './App.css';
+import ReactGA from "react-ga";
 // import axios from 'axios';
 import Pages from "./components/Pages";
 
 const App = () => {
 
-  // const [values, setData] = useState();
-  // const getData = () => {
-  //   axios.get('/api')
-  //     .then(res => setData(res))
-  //     .catch(err => console.log(err)) 
-  // }
-  // console.log(values)
-
-  // React.useEffect(() => getData(), []);
-
-
-
+  useEffect(() => {
+    ReactGA.initialize('UA-151021167-1');
+  }, [])
+  
   return (
-    <div>
     <Pages />
-    </div>
   );
 }
 
